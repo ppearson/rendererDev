60c9d40b0a8f2eea25e55635ed8d6a84f05991c3
Peter Pearson <peter.pearson@gmail.com>
2016-06-11 10:35:11 +1200

    Improvements to Nuke ImagineRender node

--- nuke/imagineRender/src/imagine_render.cpp
+++ nuke/imagineRender/src/imagine_render.cpp
@@ -19,7 +19,6 @@
 #include "objects/mesh.h"
 #include "objects/primitives/sphere.h"
 #include "lights/point_light.h"
-#include "lights/spot_light.h"
 #include "geometry/standard_geometry_instance.h"
 #include "geometry/standard_geometry_operations.h"
 #include "materials/standard_material.h"
@@ -33,8 +32,6 @@
 #include "global_context.h"
 #include "output_context.h"
 
-using namespace DD::Image;
-
 static const char* const CLASS = "imagine_render";
 static const char* const HELP = "Imagine_render";
 
@@ -45,27 +42,61 @@ const char* const RENDER_TYPE[] =
 	0
 };
 
-class ImagineRenderIop : public Iop
+const char* const PROJECTION_TYPE[] =
+{
+	"perspective",
+	"spherical",
+	"spherical omni-dir",
+	"fisheye",
+	"fisheye omni-dir",
+	0
+};
+
+const char* const ANTI_ALIASING_TYPE[] =
+{
+	"1",
+	"4",
+	"9",
+	"16",
+	"25",
+	"36",
+	"49",
+	"64",
+	"81",
+	0
+};
+
+class ImagineRenderIop : public DD::Image::Iop, public RaytracerHost
 {
 public:
-	enum eRenderType
+	enum RenderType
 	{
 		eGlobalIllumination,
 		eDirectIllumination
 	};
 
+	enum ProjectionType
+	{
+		ePerspective,
+		eSpherical,
+		eSphericalOmniDir,
+		eFisheye,
+		eFisheyeOmniDir
+	};
+
+	// Nuke stuff
 	ImagineRenderIop(Node* node);
 
-	virtual void knobs(Knob_Callback callback);
+	virtual void knobs(DD::Image::Knob_Callback callback);
 	virtual void _validate(bool for_real);
 	virtual void _request(int x, int y, int r, int t, DD::Image::ChannelMask m, int n);
 
 	virtual void append(DD::Image::Hash &hash);
 
-	virtual void engine(int y, int x, int r, ChannelMask channels, DD::Image::Row& out);
+	virtual void engine(int y, int x, int r, DD::Image::ChannelMask channels, DD::Image::Row& out);
 
 	const char* input_label(int n, char*) const;
-	virtual bool test_input(int index, Op* pOp) const;
+	virtual bool test_input(int index, DD::Image::Op* pOp) const;
 
 	int minimum_inputs() const
 	{
@@ -81,36 +112,43 @@ public:
 	const char* Class() const { return CLASS; }
 	const char* node_help() const { return HELP; }
 	const char* displayName() const { return "ImagineRender"; }
-	static const Iop::Description d;
+	static const DD::Image::Iop::Description d;
+
+
+	// Imagine stuff
+	virtual void tileDone(const TileInfo& tileInfo, unsigned int threadID);
 
 protected:
+// Imagine Stuff
+	::Scene				m_scene;
 
-	::Scene			m_scene;
+	Params				m_renderSettings;
 
-	Params			m_renderSettings;
+	OutputImage*		m_pOutputImage;
+	Raytracer*			m_pRaytracer;
 
-	OutputImage*	m_pOutputImage;
-	Raytracer*		m_pRaytracer;
 
-	Channel			m_channels[4];
-	FormatPair		m_formats;
+// Nuke stuff
+	DD::Image::Channel		m_channels[4];
+	DD::Image::FormatPair	m_formats;
 
-	DD::Image::Lock	m_lock;
-	bool			m_firstEngine;
+	DD::Image::Lock		m_lock;
+	bool				m_firstEngine;
 
 	////
 
-	eRenderType		m_renderType;
+	// knob settings
+	RenderType			m_renderType;
+	ProjectionType		m_projectionType;
 
+	int					m_antiAliasing;
+	bool				m_ambientOcclusion;
 
+	int					m_globalIlluminationSamples;
 
-	bool			m_ambientOcclusion;
-	bool			m_antiAliasing;
-
-	int				m_globalIlluminationSamples;
-
-	unsigned int	m_imageWidth;
-	unsigned int	m_imageHeight;
+	// cached values
+	unsigned int		m_imageWidth;
+	unsigned int		m_imageHeight;
 
 	/// hashes
 	DD::Image::Hash		m_cameraHash;
@@ -118,15 +156,18 @@ protected:
 	DD::Image::Hash		m_geometryHash;
 };
 
-
-//// class impl
+//
+using namespace DD::Image;
 
 ImagineRenderIop::ImagineRenderIop(Node* node) : Iop(node), m_pOutputImage(NULL), m_pRaytracer(NULL), m_firstEngine(true),
-	m_renderType(eDirectIllumination), m_ambientOcclusion(false), m_antiAliasing(false), m_globalIlluminationSamples(64)
+	m_renderType(eDirectIllumination), m_ambientOcclusion(false), m_antiAliasing(1), m_globalIlluminationSamples(64)
 {
 	m_cameraHash = 0;
 	m_lightsHash = 0;
 	m_geometryHash = 0;
+
+	// number of threads to use for geo tess/building and accel building
+	GlobalContext::instance().setWorkerThreads(8);
 }
 
 void ImagineRenderIop::knobs(Knob_Callback callback)
@@ -139,13 +180,15 @@ void ImagineRenderIop::knobs(Knob_Callback callback)
 	Enumeration_knob(callback, (int*)&m_renderType, RENDER_TYPE, "render_type", "render type");
 	Newline(callback);
 
+	Enumeration_knob(callback, (int*)&m_projectionType, PROJECTION_TYPE, "projection_type", "projection type");
+	Newline(callback);
+
+	Enumeration_knob(callback, (int*)&m_antiAliasing, ANTI_ALIASING_TYPE, "anti_aliasing", "anti aliasing");
+
 	Bool_knob(callback, &m_ambientOcclusion, "ambient_occlusion", "ambient occlusion");
-	Bool_knob(callback, &m_antiAliasing, "anti_aliasing", "anti aliasing");
 
 	Int_knob(callback, &m_globalIlluminationSamples, IRange(1, 512), "gi_samples", "GI Samples");
 	SetFlags(callback, DD::Image::Knob::SLIDER);
-
-
 }
 
 void ImagineRenderIop::_validate(bool for_real)
@@ -207,12 +250,17 @@ void ImagineRenderIop::_validate(bool for_real)
 	// set lazy mode
 	m_scene.setLazy(true);
 
-
 	unsigned int threads = System::getNumberOfCores();
 
 	m_pRaytracer = new Raytracer(m_scene, m_pOutputImage, m_renderSettings, false, threads);
+	m_pRaytracer->setHost(this);
 	m_pRaytracer->setAmbientColour(Colour3f(0.35f, 0.35f, 0.35f));
 	m_pRaytracer->setExtraChannels(0);
+
+	// for the moment we can map this directly with a shift (i.e. enum 0 == 1 sample per pixel squared)
+	m_renderSettings.add("antiAliasing", m_antiAliasing + 1);
+
+	m_pRaytracer->initialise(m_pOutputImage, m_renderSettings);
 }
 
 void ImagineRenderIop::_request(int x, int y, int r, int t, DD::Image::ChannelMask m, int n)
@@ -233,9 +281,9 @@ void ImagineRenderIop::append(DD::Image::Hash &hash)
 
 void ImagineRenderIop::buildScene()
 {
-	Op* pInput0 = input(0);
-	Op* pInput1 = input(1);
-	Op* pInput2 = input(2);
+	Op* pInput0 = Op::input(0);
+	Op* pInput1 = Op::input(1);
+	Op* pInput2 = Op::input(2);
 
 	if (dynamic_cast<CameraOp*>(pInput0))
 	{
@@ -246,21 +294,44 @@ void ImagineRenderIop::buildScene()
 		DD::Image::Knob* pTrans = pCamera->knob("translate");
 		DD::Image::Knob* pRotate = pCamera->knob("rotate");
 
-		Point position(pTrans->get_value(0), pTrans->get_value(1), pTrans->get_value(2));
+		Vector position(pTrans->get_value(0), pTrans->get_value(1), pTrans->get_value(2));
 		Vector rotation(pRotate->get_value(0), pRotate->get_value(1), pRotate->get_value(2));
 
-		Camera* pImagineCamera = new Camera(position);
-		pImagineCamera->setRotation(rotation);
+		// Because we're currently using Imagine in Embedded mode, we have to use baked 4x4 transforms
+		::Matrix4 transform;
+		transform.translate(position);
+		transform.rotate(rotation, ::Matrix4::eYXZ);
+
+		// TODO: get 4x4 transform as well
+
+		Camera* pImagineCamera = new Camera();
+		pImagineCamera->transform().setCachedTransform(transform);
 
 		DD::Image::Knob* pFocalLength = pCamera->knob("focal");
-		if (pFocalLength)
+		DD::Image::Knob* pHAperture = pCamera->knob("haperture");
+		if (pFocalLength && pHAperture)
 		{
 			float focalLength = pFocalLength->get_value(0);
+			float horizAperture = pHAperture->get_value(0);
 
-			pImagineCamera->setFOV(90.0f - focalLength);
+			// work out the actual FOV (assuming perspective for the moment)
+			// TODO: do we need diagonal, as this doesn't match Nuke?
+			float fov = 2.0f * std::atan(horizAperture / (2.0f * focalLength));
+			fov *= 180.0f / kPI;
+
+			pImagineCamera->setFOV(fov);
 		}
 
-		m_scene.setViewCamera(pImagineCamera);
+		if (m_projectionType == eSpherical)
+		{
+			pImagineCamera->setProjectionType(Camera::eSpherical);
+		}
+		else if (m_projectionType == eFisheye)
+		{
+			pImagineCamera->setProjectionType(Camera::eFisheye);
+		}
+
+		m_scene.setDefaultCamera(pImagineCamera);
 	}
 
 	DD::Image::Hash geometryHash = -1;
@@ -284,7 +355,6 @@ void ImagineRenderIop::buildScene()
 		geometryHash.append(geoList.size());
 		geometryHash.append(pGeo->hash(DD::Image::Group_Points));
 	}
-
 
 	// now add the lights
 	m_scene.freeLights();
@@ -336,35 +406,7 @@ void ImagineRenderIop::buildScene()
 					spot = true;
 			}
 
-			if (spot)
-			{
-				float coneAngle = 40.0f;
-				float penAngle = 7.0f;
-				DD::Image::Knob* pConeAngle = pLight->knob("cone_angle");
-				if (pConeAngle)
-					coneAngle = pConeAngle->get_value();
-				DD::Image::Knob* pPenAngle = pLight->knob("cone_penumbra_angle");
-				if (pPenAngle)
-					penAngle = pPenAngle->get_value();
-
-				if (coneAngle > 57.0f)
-					coneAngle = 56.0f;
-
-				SpotLight* pSP = new SpotLight();
-				pSP->setConeAngle(coneAngle);
-				pSP->setPenumbraAngle(coneAngle + penAngle);
-
-				pNewLight = static_cast<Light*>(pSP);
-
-				::Matrix4 spotRotate;
-				spotRotate.setRotationX(90.0f);
-
-				rotation = spotRotate.transform(rotation);
-			}
-			else
-			{
-				pNewLight = new PointLight();
-			}
+			pNewLight = new PointLight();
 
 			pNewLight->setPosition(position);
 			pNewLight->setRotation(rotation);
@@ -412,11 +454,10 @@ void ImagineRenderIop::buildScene()
 				if (pUVs && pUVs->attribute && pUVs->attribute->size())
 					haveUVs = true;
 
-
-
 				if (object.material)
 				{
-					// create a new material type which reads from an Iop...
+					// create a new material type which reads from an Iop and brute-force reads
+					// the full texture ahead of time...
 
 					Iop* pNukeMaterial = object.material;
 
@@ -428,6 +469,7 @@ void ImagineRenderIop::buildScene()
 					float red = pNukeMaterial->at(0, 0, Chan_Red);
 					float green = pNukeMaterial->at(0, 0, Chan_Green);
 					float blue = pNukeMaterial->at(0, 0, Chan_Blue);
+					float alpha = pNukeMaterial->at(0, 0, Chan_Alpha);
 
 					if (red == 0.0f && green == 0.0f && blue == 0.0f)
 					{
@@ -479,12 +521,13 @@ void ImagineRenderIop::buildScene()
 
 						// hack to check for null input - Nuke's creating a default black Iop when there isn't one...
 
-						if (red == 0.0f && green == 0.0f && blue == 0.0f)
+						if (red == 0.0f && green == 0.0f && blue == 0.0f && alpha == 0.0f)
 						{
+							// there's a black material attached - possibly the default, so there isn't really a
+							// material attached...
+							// there's no colour - for the moment set a default so it's obvious what's going on.
 							pNewMat = new StandardMaterial();
-							pNewMat->setDiffuseColour(Colour3f(0.7f, 0.7f, 0.7f));
-
-							pNewMat->setReflection(0.5f);
+							pNewMat->setDiffuseColour(Colour3f(0.6f, 0.6f, 0.0f));
 						}
 						else
 						{
@@ -500,17 +543,17 @@ void ImagineRenderIop::buildScene()
 				}
 				else
 				{
+					// this never actually seems to trigger as Nuke's geo always seems to
+					// have materials...
 					pNewMat = new StandardMaterial();
-					pNewMat->setDiffuseColour(Colour3f(0.7f, 0.7f, 0.7f));
-
-					pNewMat->setReflection(0.5f);
+					pNewMat->setDiffuseColour(Colour3f(0.6f, 0.6f, 0.6f));
 				}
 
 				Material* pMat = static_cast<Material*>(pNewMat);
 
-				StandardGeometryInstance* pNewGeomInstance = new StandardGeometryInstance();
+				StandardGeometryInstance* pNewGeoInstance = new StandardGeometryInstance();
 
-				std::vector<Point>& points = pNewGeomInstance->getPoints();
+				std::vector<Point>& points = pNewGeoInstance->getPoints();
 
 				const PointList* geomPoints = object.point_list();
 				unsigned int numPoints = geomPoints->size();
@@ -530,7 +573,7 @@ void ImagineRenderIop::buildScene()
 					int uvGroupType = pUVs->group;
 					unsigned int numUVs = pUVs->attribute->size();
 
-					std::vector<UV>& uvs = pNewGeomInstance->getUVs();
+					std::vector<UV>& uvs = pNewGeoInstance->getUVs();
 
 					for (unsigned int uvIndex = 0; uvIndex < numUVs; uvIndex++)
 					{
@@ -547,7 +590,8 @@ void ImagineRenderIop::buildScene()
 				aPolyOffsets.reserve(numFaces);
 
 				std::vector<uint32_t>& aPolyIndices = pNewGeoInstance->getPolygonIndices();
-				aPolyIndices.resize(numIndices);
+
+				unsigned int polyOffsetLast = 0;
 
 				for (unsigned int faceIndex = 0; faceIndex < numFaces; faceIndex++)
 				{
@@ -557,19 +601,15 @@ void ImagineRenderIop::buildScene()
 					unsigned int numSubFaces = prim->faces();
 					if (numSubFaces == 1)
 					{
-						Face newFace(numVertices);
+						aPolyOffsets.push_back(numVertices + polyOffsetLast);
 
 						for (unsigned int vertexIndex = 0; vertexIndex < numVertices; vertexIndex++)
 						{
 							unsigned int thisVertexIndex = prim->vertex(vertexIndex);
-
-							newFace.addVertex(thisVertexIndex);
-							newFace.addUV(thisVertexIndex);
+							aPolyIndices.push_back(thisVertexIndex);
 						}
 
-						newFace.calculateNormal(pNewGeomInstance);
-
-						faces.push_back(newFace);
+						polyOffsetLast += numVertices;
 					}
 					else
 					{
@@ -579,31 +619,35 @@ void ImagineRenderIop::buildScene()
 							unsigned int* pSubFaceVertices = new unsigned int[numSubFaceVertices];
 							prim->get_face_vertices(subFace, pSubFaceVertices);
 
-							Face newFace(numSubFaceVertices);
+							aPolyOffsets.push_back(numSubFaceVertices + polyOffsetLast);
 
 							for (unsigned int vertexIndex = 0; vertexIndex < numSubFaceVertices; vertexIndex++)
 							{
 								unsigned int thisVertexIndex = pSubFaceVertices[numSubFaceVertices - 1 - vertexIndex];
 
-								newFace.addVertex(thisVertexIndex);
-								newFace.addUV(thisVertexIndex);
+								aPolyIndices.push_back(thisVertexIndex);
 							}
+
+							polyOffsetLast += numSubFaceVertices;
 
 							if (pSubFaceVertices)
 								delete pSubFaceVertices;
-
-							newFace.calculateNormal(pNewGeomInstance);
-
-							faces.push_back(newFace);
 						}
 					}
 				}
 
-				pNewGeomInstance->setHasPerVertexUVs(haveUVs);
+				if (haveUVs)
+				{
+					// for the moment, we can cheat here and pass in the point indices as UV indices
+					// which works for simple shapes, but obviously need to be changed at some point...
+					pNewGeoInstance->setUVIndices(aPolyIndices);
+					pNewGeoInstance->setHasPerVertexUVs(true);
+				}
+
 
 				Mesh* pMesh = new Mesh();
 
-				pMesh->setGeometryInstance(pNewGeomInstance);
+				pMesh->setGeometryInstance(pNewGeoInstance);
 				pMesh->setMaterial(pMat);
 
 				m_scene.addObject(pMesh, false, false);
@@ -621,8 +665,6 @@ void ImagineRenderIop::engine(int y, int x, int r, ChannelMask channels, DD::Ima
 		DD::Image::Guard guard(m_lock);
 		if (m_firstEngine)
 		{
-			GlobalContext::instance().setWorkerThreads(8);
-
 			m_pRaytracer->renderScene(1.0f, NULL);
 
 			m_pOutputImage->normaliseProgressive();
@@ -631,7 +673,6 @@ void ImagineRenderIop::engine(int y, int x, int r, ChannelMask channels, DD::Ima
 			m_firstEngine = false;
 		}
 	}
-
 
 	float* p[4];
 	p[0] = out.writable(Chan_Red);
@@ -681,6 +722,12 @@ bool ImagineRenderIop::test_input(int index, Op* pOp) const
 	}
 
 	return false;
+}
+
+void ImagineRenderIop::tileDone(const TileInfo& tileInfo, unsigned int threadID)
+{
+	// TODO: try and do something useful with this, i.e. working out that an entire row of tiles is done,
+	//       so we can return the corresponding scanlines to Nuke
 }
 
 static Iop* build(Node* node)
